8f70c80bd01cf156ca03e852a263a7e1b986797e
Pranav Verma <142308925+pranav89624@users.noreply.github.com>
2024-09-20 21:54:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
+// Creating Functon To be Trigged when Tap to Button is Clicked.
+// ------------------------------------------
 function process(){
+     
+     // playing audio on taping the Button.
+     // -------------------------------------------
      var audio = new Audio('dice-95077.mp3');
      audio.play();
 
-     
+     // Selecting The Random nuber for the Player 1's Dice .
+     // --------------------------------------------
      var randomNumber1 = Math.floor(Math.random()*6)+1;
      var imageSrc = "images/dice"+randomNumber1+".png";
 
-
+     // Assigning the value of the image to the Player 1's Dice.
+     // --------------------------------------------
      document.querySelector("img.img1").setAttribute("src",imageSrc);
 
-
+     // Selecting The Random nuber for the Player 2's Dice .
+     // --------------------------------------------
      var randomNumber2 = Math.floor(Math.random()*6)+1;
      var imageSrc2 = "images/dice"+randomNumber2+".png";
 
+     // Assigning the value of the image to the Player 2's Dice.
+     // --------------------------------------------
      document.querySelector("img.img2").setAttribute("src",imageSrc2);
 
-
-     if (randomNumber1>randomNumber2){
+     // Decision Making.
+     // --------------------------------------------
+     if (randomNumber1>randomNumber2){   //--> Player 1's wining Condition.
      document.querySelector(".result").innerHTML = "Player 1 win ! 🚩 "
      }
-     else if (randomNumber1<randomNumber2){
+     else if (randomNumber1<randomNumber2){   //--> Player 2's wining Condition.
           document.querySelector(".result").innerHTML = "Player 2 win ! 🚩 "
      }
-     else{
+     else{     //--> Condition for Draw.
           document.querySelector(".result").innerHTML = "It's a Draw ! 🚩"
      }
 }
